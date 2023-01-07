68c41cbbb1c97322f9adc74b99cbd52cac364363
DockFrankenstein <franciszekpyrc@gmail.com>
2023-01-07 08:56:30 +0100

    Added basic interaction

---
+++ Assets/Scripts/Interaction/CharacterInteraction.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Game.Interaction
+{
+    public class CharacterInteraction : MonoBehaviour
+    {
+        [SerializeField] Transform interactionPoint;
+        [SerializeField] Vector3 offset;
+        [SerializeField] float interactLength = 2f;
+        [SerializeField] LayerMask interactableLayer;
+
+        bool _didHit;
+        RaycastHit _raycastHit;
+        IInteractable _hitInteractable;
+
+        private void FixedUpdate()
+        {
+            _didHit = Physics.Raycast(transform.position + offset, Vector3.down, out _raycastHit, interactLength, interactableLayer);
+            _hitInteractable = _raycastHit.transform?.GetComponent<IInteractable>();
+            _didHit = _hitInteractable != null;
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void OnDrawGizmos()
+        {
+            const float arrowHeadLengthMultiplier = 0.2f;
+
+            if (interactionPoint == null)
+                return;
+
+            float arrowHeadLength = interactLength * arrowHeadLengthMultiplier;
+
+            Gizmos.color = _didHit ? Color.green : Color.red;
+            Vector3 startPos = interactionPoint.position + offset;
+            Vector3 endPos = startPos + Vector3.down * interactLength;
+            Gizmos.DrawLine(startPos, endPos);
+            Gizmos.DrawLine(endPos, endPos + new Vector3(arrowHeadLength, arrowHeadLength, 0f));
+            Gizmos.DrawLine(endPos, endPos + new Vector3(-arrowHeadLength, arrowHeadLength, 0f));
+            Gizmos.DrawLine(endPos, endPos + new Vector3(0f, arrowHeadLength, arrowHeadLength));
+            Gizmos.DrawLine(endPos, endPos + new Vector3(0f, arrowHeadLength, -arrowHeadLength));
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Interaction
+{
+    public class Interactable : MonoBehaviour, IInteractable
+    {
+        public UnityEvent OnInteract;
+
+        public virtual void Interact()
+        {
+            OnInteract.Invoke();
+        }
+    }
+}
