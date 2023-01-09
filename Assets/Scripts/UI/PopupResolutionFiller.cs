98c0f1af8b1744111187646fcc8138da387e60aa
DockFrankenstein <franciszekpyrc@gmail.com>
2023-01-09 18:30:42 +0100

    Created a basic menu

---
+++ Assets/Scripts/UI/PopupResolutionFiller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Linq;
+
+namespace Game.UI
+{
+    public class PopupResolutionFiller : MonoBehaviour
+    {
+        [SerializeField] OptionsPopup target;
+
+        private void Reset()
+        {
+            target = GetComponent<OptionsPopup>();
+        }
+
+        private void Awake()
+        {
+            target.values = Screen.resolutions
+                .Select(x => $"{x.width}x{x.height}")
+                .ToArray();
+        }
+    }
+}
